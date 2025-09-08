e152c0d255e0c1a4e36c20d890b9ab6aab3f5ed0
Hesam V. <hesam.vahib@gmail.com>
2025-09-08 15:54:43 +0300

    add some eligible origins to call api

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import jobs
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "https://applai-fi.vercel.app/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(jobs.router)
